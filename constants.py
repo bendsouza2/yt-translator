3b4c0fb060628ea30983dd367b197260e1b48102
bendsouza <bendsouza2@gmail.com>
2024-02-23 20:13:04 +0000

    closed #1 #4 #5

--- constants.py
+++ constants.py
@@ -9,12 +9,23 @@ class Prompts:
     SENTENCE_TRANSLATOR = "Translate the following sentence from Spanish to English: {sentence}"
     WORD_TRANSLATOR = "What does the following Spanish word mean in English: '{word}'"
     SYSTEM_MESSAGE = "You are a helpful assistant designed to help a user learn Spanish"
-    IMAGE_GENERATOR = "generar una imagen relacionada con la palabra {word} en el contexto de la frase {sentence}"
+    # IMAGE_GENERATOR = "generar una imagen relacionada con la palabra {word} en el contexto de la frase {sentence}"
+    # IMAGE_GENERATOR2 = "Genera una imagen de un {word}."
+    IMAGE_GENERATOR = "{sentence}."
 
 
 class URLs:
     DICTIONARY_URL = "https://lexicala1.p.rapidapi.com/search"
     DICTIONARY_HOST = "lexicala1.p.rapidapi.com"
+
+
+class ModelTypes:
+    DALLE_MODEL = "dall-e-2"
+    GPT_MODEL = "gpt-3.5-turbo"
+
+
+class VideoSettings:
+    IMAGE_SIZE = "1024x1024"
 
 
 WORD_LIST_PATH = os.getcwd() + "/top-10000-spanish-words.txt"
