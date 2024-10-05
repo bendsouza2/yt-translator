23d24193790043fc136393473334252b2fdd03bb
bendsouza <bendsouza2@gmail.com>
2024-10-05 21:05:09 +0100

    fixed testss

--- python/tests/test_functions.py
+++ python/tests/test_functions.py
@@ -4,7 +4,7 @@ import unittest
 import os
 from unittest.mock import patch
 
-from word_generator import Audio
+from python.word_generator import Audio
 
 
 class TestWordGenerator(unittest.TestCase):
@@ -14,16 +14,16 @@ class TestWordGenerator(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.mock_get = patch("word_generator.requests.get").start()
-        cls.mock_google_translator = patch("word_generator.GoogleTranslator").start()
-        cls.mock_remove_word_from_file = patch("word_generator.Audio.remove_word_from_file").start()
+        cls.mock_get = patch("python.word_generator.requests.get").start()
+        cls.mock_google_translator = patch("python.word_generator.GoogleTranslator").start()
+        cls.mock_remove_word_from_file = patch("python.word_generator.Audio.remove_word_from_file").start()
 
         # can remove mock attributes for these 5 methods once I write unit tests to test them in controlled env
-        cls.mock_srt_file = patch("word_generator.Audio.generate_srt_file").start()
-        cls.mock_tts = patch("word_generator.Audio.text_to_speech").start()
-        cls.mock_get_audio_duration = patch("word_generator.Audio.get_audio_duration").start()
-        cls.mock_get_total_syllable_count = patch("word_generator.Audio.get_total_syllable_count_spanish").start()
-        cls.mock_generate_srt_file = patch("word_generator.Audio.echogarden_generate_subtitles").start()
+        cls.mock_srt_file = patch("python.word_generator.Audio.generate_srt_file").start()
+        cls.mock_tts = patch("python.word_generator.Audio.text_to_speech").start()
+        cls.mock_get_audio_duration = patch("python.word_generator.Audio.get_audio_duration").start()
+        cls.mock_get_total_syllable_count = patch("python.word_generator.Audio.get_total_syllable_count_spanish").start()
+        cls.mock_generate_srt_file = patch("python.word_generator.Audio.echogarden_generate_subtitles").start()
 
         cls.mock_google_translator.return_value.translate.return_value = "Translated sentence"
         cls.audio = Audio(
@@ -46,14 +46,14 @@ class TestWordGenerator(unittest.TestCase):
         random_word = self.audio.get_random_word()
         self.assertIn(random_word, self.audio.text_file)
 
-    @patch("word_generator.requests.get")
+    @patch("python.word_generator.requests.get")
     def test_test_real_word(self, mock_get):
         #TODO need to amend these two tests --> delete this function from the Audio class and test the LangVerif instead
         mock_get.return_value.status_code = 200
         mock_get.return_value.json.return_value = {"n_results": 1}
         self.assertTrue(self.audio.test_real_word("apple"))
 
-    @patch("word_generator.requests.get")
+    @patch("python.word_generator.requests.get")
     def test_test_real_word_fake(self, mock_get):
         mock_get.return_value.status_code = 200
         mock_get.return_value.json.return_value = {"n_results": 0}
