fe1709ee00ef9cebd18d4b9eab8b169e29e718f2
bendsouza <bendsouza2@gmail.com>
2024-10-14 18:55:56 +0100

    update constants

--- python/constants.py
+++ python/constants.py
@@ -12,7 +12,8 @@ class Prompts:
     SENTENCE_GENERATOR = "escribe una frase de ejemplo sobre el uso de la palabra {word}"
     SENTENCE_TRANSLATOR = "Translate the following sentence from Spanish to English: {sentence}"
     WORD_TRANSLATOR = "What does the following Spanish word mean in English: '{word}'"
-    SYSTEM_MESSAGE = "You are a helpful assistant designed to help a user learn Spanish"
+    SYSTEM_MESSAGE = "You are a helpful assistant designed to help a user learn languages"
+    IMAGE_GENERATOR = "Generate an image to match the following Spanish sentence {sentence}"
 
 
 @dataclass
@@ -23,16 +24,20 @@ class URLs:
 
 @dataclass
 class ModelTypes:
-    DALLE_MODEL = "dall-e-2"
+    DALLE_MODEL = "dall-e-3"
     GPT_MODEL = "gpt-3.5-turbo"
 
 
 @dataclass
 class VideoSettings:
-    IMAGE_SIZE: Literal["1024x1024"] = "1024x1024"
+    SQUARE: Literal["1024x1024"] = "1024x1024"
+    VERTICAL: Literal["1024x1792"] = "1024x1792"
 
 
 class Paths:
     WORD_LIST_PATH = "/Users/bendsouza/PycharmProjects/yt_translator/top-10000-spanish-words.txt"
     SUBTITLE_DIR_PATH = "/Users/bendsouza/PycharmProjects/yt_translator/subtitles"
     AUDIO_DIR_PATH = "/Users/bendsouza/PycharmProjects/yt_translator/audio"
+    IMAGE_DIR_PATH = "/Users/bendsouza/PycharmProjects/yt_translator/images"
+    NODE_SUBS_FILE_PATH = "/Users/bendsouza/PycharmProjects/yt_translator/node/sync_subtitles.js"
+    VIDEO_DIR_PATH = "/Users/bendsouza/PycharmProjects/yt_translator/video"
