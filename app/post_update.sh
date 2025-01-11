b8bbb191a19e551baff19d5f8624f43f6b592161
bendsouza <bendsouza2@gmail.com>
2025-01-11 15:57:01 +0000

    ensure gunicorn restart when backend updates pushed

--- app/post_update.sh
+++ app/post_update.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+cd ~/yt_translator
+git pull origin main
+
 # Copy custom settings file
-if [ ! -f settings.py ]; then
+if [ ! -f video_host/settings.py ]; then
     echo "Restoring settings.py..."
     cp /home/ubuntu/settings_backup/settings.py video_host/settings.py
 fi
 
 source /home/ubuntu/yt_translator/venv/bin/activate
 python manage.py migrate
+
+sudo systemctl stop gunicorn
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
 
 
